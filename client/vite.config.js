cd7a183fb4f973c2f7ad2d8bb780474c7fbc5483
Kushal <kushalkhanal181@gmail.com>
2026-05-16 20:12:09 +0545

    Update proxy targets in Vite config to use production URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,11 +17,11 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://kushalkhanal.com.np',
         changeOrigin: true
       },
       '/uploads': {
-        target: 'http://localhost:5000',
+        target: 'https://kushalkhanal.com.np',
         changeOrigin: true
       }
     }
